31a01adcc01c4a8222b6fccec536c03099c67920
avelitj12 <avelitj12@gmail.com>
2023-12-27 15:15:25 -0700

    Add spawning of animated obstacle fish

--- main.gd
+++ main.gd
@@ -1,11 +1,31 @@
 extends Node
 
+@export var obstacle_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().call_group("obstacles", "queue_free")
+	$ObstacleTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_obstacle_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var obstacle = obstacle_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var obstacle_spawn_location = get_node("ObstaclePath/ObstacleSpawnLocation")
+	obstacle_spawn_location.progress_ratio = randf()
+
+	# Set the mob's position to a random location.
+	obstacle.position = obstacle_spawn_location.position
+
+	# Choose the velocity for the mob.
+	obstacle.linear_velocity = 400 * Vector2.LEFT
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(obstacle)
--- obstacle_fish.gd
+++ obstacle_fish.gd
@@ -3,9 +3,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.play("swim_neutral")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
