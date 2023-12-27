941d0f67d525da8337d181248415679f322883cd
avelitj12 <avelitj12@gmail.com>
2023-12-27 16:53:19 -0700

    Games start and game over screens

--- main.gd
+++ main.gd
@@ -1,18 +1,28 @@
 extends Node
 
 @export var obstacle_scene: PackedScene
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().call_group("obstacles", "queue_free")
 	$Player.start($StartPosition.position)
-	$ObstacleTimer.start()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func game_over():
+	$ScoreTimer.stop()
+	$ObstacleTimer.stop()
+	$HUD.show_game_over()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$HUD.update_score("Score: %s" % score)
+	$HUD.show_message("Get Ready")
+	$StartTimer.start()
 
 func _on_obstacle_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -30,3 +40,11 @@ func _on_obstacle_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(obstacle)
+
+func _on_start_timer_timeout():
+	$ObstacleTimer.start()
+	$ScoreTimer.start()
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score("Score: %s" % score)
