0e75a658d7c64d21e30b13eb06f42346e4f14885
avelitj12 <avelitj12@gmail.com>
2023-12-27 14:15:29 -0700

    Add animation to player fish
    Different animations for when neutral, moving up, and moving down
    
    Also quick update to movement that requires a key press per impulse

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var speed = 0
-var swim_impulse = 30
+var swim_impulse = 150
 var coasting_deceleration = 150
 var velocity = Vector2.ZERO
 
@@ -10,26 +10,30 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.animation = "swim_neutral"
+	$AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = 0
 	
-	# Get direction player is swimming
-	if Input.is_action_pressed("swim_up"):
+	# Get direction player is swimming, but require a keystoke per impulse
+	if Input.is_action_just_pressed("swim_up"):
 		direction = 1
-	if Input.is_action_pressed("swim_down"):
+	if Input.is_action_just_pressed("swim_down"):
 		direction = -1
 	
 	# Apply the swim impulse
 	speed += swim_impulse * direction
 	
 	# Apply constant drag against movement
-	if speed > 0: 
+	if speed > coasting_deceleration * delta: 
 		speed -= coasting_deceleration * delta
-	elif speed < 0:
+	elif speed < -(coasting_deceleration * delta):
 		speed += coasting_deceleration * delta
+	else:
+		speed = 0
 	
 	# Calc vector and change position
 	velocity = Vector2.UP * speed
@@ -38,3 +42,14 @@ func _process(delta):
 	
 	# Prevent flying off the screen
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	# When player hits wall, cancel all speed
+	if position.y == 0 || position.y == screen_size.y:
+		speed = 0
+	
+	if velocity.y > 0:
+		$AnimatedSprite2D.animation = "swim_down"
+	elif velocity.y < 0:
+		$AnimatedSprite2D.animation = "swim_up"
+	else:
+		$AnimatedSprite2D.animation = "swim_neutral"
