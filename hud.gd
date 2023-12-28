88840db62e318d0346d796affe7982ad228b7ff2
avelitj12 <avelitj12@gmail.com>
2023-12-28 08:54:29 -0700

    Fix "Survive!" message disappearing
    Also align message timer with how long game over sound is

--- hud.gd
+++ hud.gd
@@ -13,7 +13,8 @@ func show_game_over():
 	# Wait until the MessageTimer has counted down.
 	await $MessageTimer.timeout
 
-	show_message("Survive!")
+	$Message.text = "Survive!"
+	$Message.show()
 
 	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
