e77fd0142180a88a51c907b5853de9730f5fe8fb
avelitj12 <avelitj12@gmail.com>
2024-01-02 15:51:01 -0700

    Rotate player sprite based on speed
    Change sprite animation to be based on player input direction as well

--- player.gd
+++ player.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 signal hit
 
+
+var MAX_SPEED = 400
+
 var speed = 0
-var acceleration = 15
-var coasting_deceleration = 150
+var acceleration = 900
+var coasting_deceleration = 400
 var air_gravity = 400
 var velocity = Vector2.ZERO
 
@@ -33,15 +36,16 @@ func _process(delta):
 		if Input.is_action_pressed("swim_down"):
 			direction = -1
 		# Apply the swim impulse
-		speed += acceleration * direction
+		speed += acceleration * delta * direction
 		
-		# Apply constant drag against movement
-		if speed > coasting_deceleration * delta: 
-			speed -= coasting_deceleration * delta
-		elif speed < -(coasting_deceleration * delta):
-			speed += coasting_deceleration * delta
-		else:
-			speed = 0
+		# Apply constant drag against movement if no direction input
+		if not direction:
+			if speed > coasting_deceleration * delta: 
+				speed -= coasting_deceleration * delta
+			elif speed < -(coasting_deceleration * delta):
+				speed += coasting_deceleration * delta
+			else:
+				speed = 0
 	
 	# Calc vector and change position
 	velocity = Vector2.UP * speed
@@ -54,13 +58,19 @@ func _process(delta):
 	# When player hits bottom, cancel all speed
 	if position.y == screen_size.y:
 		speed = 0
+	else:
+		speed = clamp(speed, -MAX_SPEED, MAX_SPEED)
 	
-	if velocity.y > 0 && not in_air:
+	# Sprite animation based on direction of player input (neutral in air)
+	if direction < 0 && not in_air:
 		$AnimatedSprite2D.animation = "swim_down"
-	elif velocity.y < 0 && not in_air:
+	elif direction > 0 && not in_air:
 		$AnimatedSprite2D.animation = "swim_up"
 	else:
 		$AnimatedSprite2D.animation = "swim_neutral"
+		
+	# Rotation based on velocity
+	rotation = -PI / 2 * speed / (MAX_SPEED * 1.5)
 
 
 func _on_body_entered(body):
