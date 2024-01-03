8b95880a845706a884e7423234076487cb40c081
avelitj12 <avelitj12@gmail.com>
2024-01-03 12:35:02 -0700

    Make game built in 1280x720 and able to scale
    Switch to 16:9 aspect ratio
    
    y values increase by 1.5 (720/480)
    
    x values increase by 16/9 (1280/720)
    
    Player speed is in the y direction
    
    Obstacle speed is in the x direction
    
    Change obstacles to have VisibleOnScreenNotifier instead of enabler so we have more control over obstacles
    
    Then tweak everything to work together well in the new project canvas size

--- obstacle_wall.gd
+++ obstacle_wall.gd
@@ -4,5 +4,6 @@ func set_gap(gap_size):
 	$TopGate.translate(Vector2(0, -gap_size/2))
 	$BottomGate.translate(Vector2(0, gap_size/2))
 
-func _on_visible_on_screen_enabler_2d_screen_exited():
-	queue_free()
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if position.x < -$VisibleOnScreenNotifier2D.get_rect().size.x/2:
+		queue_free()
