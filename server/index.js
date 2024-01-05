f9d95b95d7f2c400b95a6a374d409a3af7c3563c
Cameron Avelis <cmavelis@gmail.com>
2024-01-04 22:14:17 -0800

    sample server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const Koa = require('koa');
 const logger = require('koa-logger');
-const koaBody = require('koa-body');
-
+const { koaBody } = require('koa-body');
 
 const app = new Koa();
 app.use(logger());
@@ -12,14 +11,14 @@ app.use(async function(ctx, next) {
     if ('POST' !== ctx.method) return await next();
     const body = ctx.request.body;
     console.log('body:', body)
-    ctx.body = 'POST'
+    ctx.body = 'POSTed'
 });
 
 // GET to get scoreboard
 app.use(async function(ctx, next) {
     if ('GET' !== ctx.method) return await next();
     // get scoreboard
-    ctx.body = 'GET'
+    ctx.body = 'GETted'
 });
 
 app.listen(3000);
