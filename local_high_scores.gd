476e132dcfe41b492f5c03ca2f9b8b8d3ec455a5
avelitj12 <avelitj12@gmail.com>
2024-01-05 16:50:18 -0700

    Implement the local scoreboard
    Re-structure HUD to contain multiple screens to switch between
    -MainMenu
    -GameScreen
    -LocalLeaderboard
    -HighScoreEntry
    
    Most screens are straightforward components that have values changed, except LocalLeaderBoard
    
    LocalLeaderBoard has an empty HighScores child that populates with the loaded high scores
    
    high scores are managed by the local_high_scores.gd script, Autoloaded as LocalHighScores and usable as a (global) singleton
    
    High scores are saved user://local_high_scores.json which on Windows is %APPDATA%\Godot\app_userdata\flappy_fish\local_high_scores.json

---
+++ local_high_scores.gd
@@ -0,0 +1,57 @@
+extends Node
+
+const SCORE_SAVE_PATH = "user://local_high_scores.json"
+
+var version = 1
+
+const HIGH_SCORE_COUNT_LIMIT = 10
+
+var scores = []
+
+func save():
+	if scores.size() > HIGH_SCORE_COUNT_LIMIT:
+		scores.resize(HIGH_SCORE_COUNT_LIMIT)
+	
+	var file = FileAccess.open(SCORE_SAVE_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(scores))
+	file.close()
+
+func load():
+	var file = FileAccess.open(SCORE_SAVE_PATH, FileAccess.READ)
+	if file:
+		var json = JSON.new()
+		var data = json.parse_string(file.get_as_text())
+		file.close()
+		if typeof(data) == TYPE_ARRAY:
+			scores = data
+		else:
+			printerr("Corrupted data!")
+	else:
+		printerr("No saved data!")
+		
+func is_high_score(score):
+	return HIGH_SCORE_COUNT_LIMIT > get_score_rank(score)
+
+func get_score_rank(score):
+	var rank = HIGH_SCORE_COUNT_LIMIT
+	if scores.size() == 0:
+		rank = 0
+	else:
+		rank = scores.size()
+		for entry in scores:
+			var entry_score = int(entry["Score"])
+			if int(score) > entry_score:
+				rank = scores.find(entry)
+				break
+			if int(score) == entry_score:
+				rank = scores.find(entry) + 1
+	return rank
+
+func submit_score(name, score):
+	var rank = get_score_rank(score)
+	
+	# Check if it's a high score (rank less than max count)
+	if rank < HIGH_SCORE_COUNT_LIMIT:
+		scores.insert(rank, {"Name": name, "Score": score})
+		save()
+		return true
