79cec0d9acbf9cf1062786706d20d2ba717afa1e
Cameron Avelis <cmavelis@gmail.com>
2024-01-04 22:23:35 -0800

    pseudocode out

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const { koaBody } = require('koa-body');
 
+const sampleScores = require('./scoreboard-sample.json')
+
 const app = new Koa();
 app.use(logger());
 app.use(koaBody({ multipart: true }));
@@ -11,14 +13,23 @@ app.use(async function(ctx, next) {
     if ('POST' !== ctx.method) return await next();
     const body = ctx.request.body;
     console.log('body:', body)
+    // JSON parse body
+    // check for name, score fields
+    // if score in top 10
+    //   add to scoreboard
+    // else
+    //   too bad
+    //
+    // return scoreboard
+
     ctx.body = 'POSTed'
 });
 
 // GET to get scoreboard
 app.use(async function(ctx, next) {
     if ('GET' !== ctx.method) return await next();
-    // get scoreboard
-    ctx.body = 'GETted'
+    // load scoreboard from local file?
+    ctx.body = sampleScores
 });
 
 app.listen(3000);
