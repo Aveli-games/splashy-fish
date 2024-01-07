3d3b56cca33f5c71c5ef5bc0c8bbe0037594f9d6
Cameron Avelis <cmavelis@gmail.com>
2024-01-07 00:31:33 -0800

    post score fn

--- autoloads/global_high_scores.gd
+++ autoloads/global_high_scores.gd
@@ -1,8 +1,10 @@
 extends Node
 
 var scores = []
+var error = false
 var http = HTTPRequest.new()
 var server_url = "https://warm-abacus-410306.uw.r.appspot.com/"
+#var server_url = "http://localhost:3000"
 
 func _ready():
 	add_child(http)
@@ -14,16 +16,17 @@ func fetch_scores():
 
 func post_score(initials: String, score: int):
 	var body = JSON.stringify({"name": initials, "score": score})
-	var error = http.request(server_url, [], HTTPClient.METHOD_POST, body)
-	if error != OK:
+	var headers = ["Content-Type: application/json"]
+	var request_error = http.request(server_url, headers, HTTPClient.METHOD_POST, body)
+	if request_error != OK:
 		push_error("An error occurred in the HTTP request.")
+		error = true
 	pass
 
 func _on_request_completed(_result, response_code, _headers, body):
 	if response_code != 200:
 		print('error getting global scores')
 		pass
-	print(body.get_string_from_utf8())
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	if json && json.has("data") && typeof(json["data"]) == TYPE_ARRAY:
 		scores = json["data"]
