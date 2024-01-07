372e716854fbf8e8aa3ba39ad7744b7952cb662d
Cameron Avelis <cmavelis@gmail.com>
2024-01-06 22:40:05 -0800

    make global autoload and organize code

--- autoloads/global_high_scores.gd
+++ autoloads/global_high_scores.gd
@@ -1,8 +1,10 @@
 extends Node
 
 func _ready():
-	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("https://warm-abacus-410306.uw.r.appspot.com/")
+	var http = HTTPRequest.new()
+	add_child(http)
+	http.request_completed.connect(_on_request_completed)
+	http.request("https://warm-abacus-410306.uw.r.appspot.com/")
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
